5e20b7cd7f1df220c45f2fee5c53fe3670d589cb
liuyfab <liuyongfeng83@gmail.com>
2021-12-11 19:04:50 -0600

    modified sql file

--- db/schema.sql
+++ db/schema.sql
@@ -22,6 +22,5 @@ CREATE TABLE employee (
   last_name VARCHAR(30),
   role_id INT,
   CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-  manager_id INT,
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+  manager_id INT
 );
